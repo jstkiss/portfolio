0942df7e959794d3bd6b7bde339a6bbbb820f65a
JSTKode <mehdi.cesa@outlook.fr>
2022-11-22 11:52:01 +0100

    update

--- src/components/competences.jsx
+++ src/components/competences.jsx
@@ -3,16 +3,12 @@ import Html from "@src/assets/img/Html.png";
 import Css from "@src/assets/img/Css.png";
 import Javascript from "@src/assets/img/Javascript.png";
 import Webpack from "@src/assets/img/Webpack.png";
-import Bootstrap from "@src/assets/img/Bootstrap.png";
 import Tailwind from "@src/assets/img/Tailwind.png";
-import Wordpress from "@src/assets/img/Wordpress.png";
 import Reactjs from "@src/assets/img/Reactjs.png";
 import Nextjs from "@src/assets/img/Nextjs.png";
 import Php from "@src/assets/img/Php.png";
 import Sql from "@src/assets/img/Sql.png";
 import Nodejs from "@src/assets/img/Nodejs.png";
-import Git from "@src/assets/img/Git.png";
-import Npm from "@src/assets/img/Npm.png";
 import photoshop from "@src/assets/img/photoshop.jpg";
 import Gitlabs from "@src/assets/img/gitlabs.png"
 import Indesign from "@src/assets/img/indesign.png"
