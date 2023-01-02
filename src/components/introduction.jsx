ae3338af28a302c330c0b6401735932e77140fcd
JSTKode <mehdi.cesa@outlook.fr>
2023-01-02 18:04:58 +0100

    update

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -25,7 +25,7 @@ const Introduction = () => {
       </div>
       <div>
         <p className="text-white pt-5 pb-3 text-sm lg:text-1xl lg:text-start xl:text-start">
-          Je suis passionné par les métiers de l’informatique,
+          Je suis passionné par les métiers de l&apos;informatique,
           l&apos;entreprenariat et l&apos;investissement, j&apos;ai choisi le
           développement web car ce domaine réunit tout ce qui me passionne. Je
           suis curieux, créatif et j&apos;aime apprendre. Ce qui me différencie,
@@ -34,16 +34,9 @@ const Introduction = () => {
         </p>
       </div>
       <div className="pt-7 flex justify-center">
-        <a href="./docs/CV_Mehdi_CESA.pdf" target="_blank" rel="noreferrer noopener" className="border-customgreen uppercase border-2 pt-2 pb-2 pl-8 pr-8 text-white font-bold cursor-pointer rounded hover:scale-125 hover:ease-in hover:duration-300 hover:text-customgreen">
+        <a href="" target="_blank" rel="noreferrer noopener" className="border-customgreen uppercase border-2 pt-2 pb-2 pl-8 pr-8 text-white font-bold cursor-pointer rounded hover:scale-125 hover:ease-in hover:duration-300 hover:text-customgreen">
           Mon cv !
         </a>
-      </div>
-      <div className="flex justify-center p-8">
-        <Iframe
-          className="h-64"
-          src="https://rive.app/community/2720-5587-rive-bot/embed"
-          allowfullscreen
-        />
       </div>
     </div>
   );
